a0f75e4bbbef65fe464302cb3a0f8aaeefeefd23
nagibaka6667@gmail.com <nagibaka6667@gmail.com>
2023-09-10 03:00:17 +0700

    v61.7(10.1.7)(10.09.2023)

--- Octo_Utility.lua
+++ Octo_Utility.lua
@@ -373,16 +373,16 @@ end)
 tinsert(E.Octo_Globals.modules, function()
 		if Octo_ToDoVars.config.CVar then
 			C_Timer.After(1, function()
-					SetCVar("nameplateShowOnlyNames", 1)
-					SetCVar("nameplateShowFriends", 1)
-					SetCVar("nameplateVerticalScale", 1)
-					-- SetCVar("nameplateShowOnlyNames", 0) --1 убирает ХП бар (френдли и энеми) лишь ctrl+V
-					-- SetCVar("nameplateShowFriends", 0)--1 отображение френдли немплейта 1вкл 0 выкл (самая полоса с именем)
-					-- SetCVar("nameplateVerticalScale", 1)--0.1 мелкий 1 дефолт 2.7 большая
+					-- SetCVar("nameplateShowOnlyNames", 1)
+					-- SetCVar("nameplateShowFriends", 1)
+					-- SetCVar("nameplateVerticalScale", 1)
+					SetCVar("nameplateShowOnlyNames", 0) --1 убирает ХП бар (френдли и энеми) лишь ctrl+V
+					SetCVar("nameplateShowFriends", 0)--1 отображение френдли немплейта 1вкл 0 выкл (самая полоса с именем)
+					SetCVar("nameplateVerticalScale", 1)--0.1 мелкий 1 дефолт 2.7 большая
 
 
 
-					SetCVar("nameplateMotion", 0) --(0 Наложение) (1 Друг над другом) stack
+					SetCVar("nameplateMotion", 1) --(0 Наложение) (1 Друг над другом) stack
 					--SetCVar("questPOI", 101)
 					SetCVar("alwaysCompareItems", 0)
 					SetCVar("rotateMinimap", 0)
