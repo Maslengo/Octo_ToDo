d7ca171824b572450547c0f50214197cb86ef06a
nagibaka6667@gmail.com <nagibaka6667@gmail.com>
2024-07-13 22:07:10 +0700

    v73.4(11.0.0)(13.07.2024)

--- Octo_SharedMedia.lua
+++ Octo_SharedMedia.lua
@@ -212,3 +212,6 @@ LibSharedMedia:Register("statusbar", "|cFF00A3FFO_23|r", [[Interface\Addons\]]..
 --Interface\Addons\]]..GlobalAddonName..[[\Media\statusbar\AtrocityUI
 LibSharedMedia:Register("statusbar", "|cFF8e7cc3Atrocity|r", [[Interface\Addons\]]..GlobalAddonName..[[\Media\statusbar\AtrocityUI\Atrocity.tga]], LibSharedMedia.LOCALE_BIT_ruRU+LibSharedMedia.LOCALE_BIT_western)
 LibSharedMedia:Register("statusbar", "|cFF8e7cc3Kait|r", [[Interface\Addons\]]..GlobalAddonName..[[\Media\statusbar\AtrocityUI\Kait.tga]], LibSharedMedia.LOCALE_BIT_ruRU+LibSharedMedia.LOCALE_BIT_western)
+
+
+LibSharedMedia:Register("statusbar", "|cFF8e7cc3Kait|r", [[Interface\Addons\]]..GlobalAddonName..[[\Media\statusbar\AtrocityUI\minecraft.tga]], LibSharedMedia.LOCALE_BIT_ruRU+LibSharedMedia.LOCALE_BIT_western)
